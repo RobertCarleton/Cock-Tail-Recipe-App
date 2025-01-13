bb1659a2f14678cafc0cab3a871e13ded14ed2e6
RobertCarleton <123363877+RobertCarleton@users.noreply.github.com>
2025-01-13 17:30:31 +0000

    Cocktail Model Added

---
+++ CocktailRecipeApp/CocktailRecipeApp/Cocktail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailRecipeApp
+{
+    public enum CocktailGlassType
+    {
+        Martini,
+        Highball,
+        Coupe,
+        Rocks,
+        Gin,
+        Lowball
+    }
+    public class Cocktail
+    {
+        public int CocktailID { get; set; }
+        public string CocktailName { get; set; }
+        public CocktailGlassType GlassType { get; set; }
+        public string CocktailDescription { get; set; }
+        public string Ingredient { get; set; }
+        public string Instructions { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsFavourite { get; set; }
+        public int Rating { get; set; }
+    }
+}
