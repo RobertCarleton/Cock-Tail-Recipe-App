2d001391d86fd3464f960c74dbe348554b6dd49c
RobertCarleton <123363877+RobertCarleton@users.noreply.github.com>
2025-01-14 00:21:28 +0000

    Database Started

---
+++ CocktailRecipeApp/CocktailRecipeApp/CocktailDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailRecipeApp
+{
+    public class CocktailDbContext : DbContext
+    {
+        //public CocktailDbContext(string databaseName) : base(databaseName)
+        public CocktailDbContext()
+        { }
+        public DbSet<Cocktail> Cocktails { get; set; }
+
+    }
+}
