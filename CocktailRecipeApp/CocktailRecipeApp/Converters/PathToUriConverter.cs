187e9cae8db5902a8f83e9269fc4409b523e0706
RobertCarleton <123363877+RobertCarleton@users.noreply.github.com>
2025-01-26 21:28:32 +0000

    Fixed External Image Import

--- CocktailRecipeApp/CocktailRecipeApp/Converters/PathToUriConverter.cs
+++ CocktailRecipeApp/CocktailRecipeApp/Converters/PathToUriConverter.cs
@@ -9,10 +9,35 @@ namespace CocktailRecipeApp.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string path)
+            if (value is string path && !string.IsNullOrWhiteSpace(path))
             {
-                // Convert the relative path to a full URI
-                return new BitmapImage(new Uri($"pack://application:,,,/{path}"));
+                try
+                {
+                    Uri imageUri;
+
+                    // Check if the path is an absolute URI (for external images)
+                    if (Uri.TryCreate(path, UriKind.Absolute, out imageUri) &&
+                        (imageUri.Scheme == Uri.UriSchemeHttp || imageUri.Scheme == Uri.UriSchemeHttps))
+                    {
+                        // External image URL (HTTP/HTTPS)
+                        return new BitmapImage(imageUri);
+                    }
+                    else if (System.IO.File.Exists(path))
+                    {
+                        // Local file path (external images stored on user's computer)
+                        return new BitmapImage(new Uri(path));
+                    }
+                    else
+                    {
+                        // Internal image (local project resources)
+                        return new BitmapImage(new Uri($"pack://application:,,,/{path}"));
+                    }
+                }
+                catch
+                {
+                    // Return a placeholder or null if the path is invalid
+                    return null;
+                }
             }
             return null;
         }
